f659e273bc9f30ac80a5477cdd91683aad43874f
Daan Gerits <daan@synadia.com>
2025-09-23 18:11:31 +0200

    fix: cleaning up the specs
    Signed-off-by: Daan Gerits <daan@synadia.com>

---
+++ test/syntax/generator.go
@@ -0,0 +1,229 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand/v2"
+
+	"github.com/lucasjones/reggen"
+	"github.com/synadia-io/connect/model"
+)
+
+const DefaultProduceSubject = "default.produce.subject"
+const DefaultConsumeSubject = "default.consume.subject.>"
+
+func generateDefaultInlet(component model.Component) (*model.Steps, error) {
+	result := model.Steps{
+		Source: &model.SourceStep{},
+		Producer: &model.ProducerStep{
+			Core: &model.ProducerStepCore{
+				Subject: DefaultProduceSubject,
+			},
+		},
+	}
+
+	// Apply component defaults
+	fields, err := getFields(component.Fields)
+	if err != nil {
+		return nil, err
+	}
+
+	result.Source.Type = component.Name
+	result.Source.Config = fields
+
+	return &result, nil
+}
+
+func generateDefaultOutlet(component model.Component) (*model.Steps, error) {
+	result := model.Steps{
+		Sink: &model.SinkStep{},
+
+		Consumer: &model.ConsumerStep{
+			Core: &model.ConsumerStepCore{
+				Subject: DefaultConsumeSubject,
+			},
+		},
+	}
+
+	// Apply component defaults
+	fields, err := getFields(component.Fields)
+	if err != nil {
+		return nil, err
+	}
+
+	result.Sink.Type = component.Name
+	result.Sink.Config = fields
+
+	return &result, nil
+}
+
+func getFields(fields []model.ComponentField) (map[string]any, error) {
+	result := make(map[string]any)
+
+	for _, f := range fields {
+		v, set, err := getFieldValue(f)
+		if err != nil {
+			return nil, fmt.Errorf("failed to generate default for field %s: %w", f.Name, err)
+		}
+		if !set {
+			continue
+		}
+
+		result[f.Name] = v
+	}
+
+	return result, nil
+}
+
+func getFieldValue(fld model.ComponentField) (any, bool, error) {
+	// -- if the field is not required, skip it
+	if !isRequired(fld) {
+		return nil, false, nil
+	}
+
+	// -- if the field has a default value, use it
+	if fld.Default != nil {
+		return fld.Default, true, nil
+	}
+
+	// -- in all other cases, we need to generate a sensible value
+	val, err := generateFieldValue(fld)
+	if err != nil {
+		return nil, false, err
+	}
+
+	return val, true, nil
+}
+
+func isRequired(fld model.ComponentField) bool {
+	if fld.Optional == nil {
+		return true
+	}
+
+	return !*fld.Optional
+}
+
+func generateFieldValue(fld model.ComponentField) (any, error) {
+	switch fld.Kind {
+	case model.ComponentFieldKindList:
+		length := rand.IntN(10)
+		result := make([]any, length)
+		for i := 0; i < length; i++ {
+			v, err := generateFieldScalarValue(fld)
+			if err != nil {
+				return nil, err
+			}
+			result[i] = v
+		}
+
+		return result, nil
+	case model.ComponentFieldKindMap:
+		length := rand.IntN(10)
+		result := make(map[string]any, length)
+		for i := 0; i < length; i++ {
+			key := "key" + fmt.Sprint(i)
+			v, err := generateFieldScalarValue(fld)
+			if err != nil {
+				return nil, err
+			}
+			result[key] = v
+		}
+		return result, nil
+
+	case model.ComponentFieldKindScalar, "":
+		return generateFieldScalarValue(fld)
+
+	default:
+		return nil, fmt.Errorf("unknown field kind %s", fld.Kind)
+	}
+}
+
+func generateFieldScalarValue(fld model.ComponentField) (any, error) {
+	switch fld.Type {
+	case model.ComponentFieldTypeInt:
+		return generateIntValue(fld)
+	case model.ComponentFieldTypeBool:
+		return rand.IntN(2)%2 == 0, nil
+	case model.ComponentFieldTypeCondition:
+		return "content().length() > 0", nil
+	case model.ComponentFieldTypeExpression:
+		return "root = {\"key\": \"value\"}", nil
+	case model.ComponentFieldTypeString:
+		return generateStringValue(fld)
+	case model.ComponentFieldTypeObject:
+		var flds []model.ComponentField
+		for _, f := range fld.Fields {
+			if f == nil {
+				continue
+			}
+
+			flds = append(flds, *f)
+		}
+
+		return getFields(flds)
+	case model.ComponentFieldTypeScanner:
+		return `{"to_the_end": {}}`, nil
+	default:
+		return nil, fmt.Errorf("unknown field type %s", fld.Type)
+	}
+}
+
+func generateIntValue(fld model.ComponentField) (any, error) {
+	var constRange model.ComponentFieldConstraintsElemRange
+	for _, c := range fld.Constraints {
+		if c.Range != nil {
+			constRange = *c.Range
+			break
+		}
+	}
+
+	minVal := 0
+	maxVal := 100
+	if constRange.Gt != nil {
+		minVal = int(math.Ceil(*constRange.Gt)) + 1
+	}
+	if constRange.Gte != nil {
+		minVal = int(math.Ceil(*constRange.Gte))
+	}
+
+	if constRange.Lt != nil {
+		maxVal = int(math.Floor(*constRange.Lt)) - 1
+	}
+	if constRange.Lte != nil {
+		maxVal = int(math.Floor(*constRange.Lte))
+	}
+
+	if minVal > maxVal {
+		return 0, fmt.Errorf("invalid range: min %d > max %d", minVal, maxVal)
+	}
+
+	return minVal + rand.IntN(maxVal-minVal), nil
+}
+
+func generateStringValue(fld model.ComponentField) (any, error) {
+	var constEnum []string
+	var constPattern string
+
+	// -- check for constraints
+	for _, c := range fld.Constraints {
+		if c.Enum != nil {
+			constEnum = c.Enum
+		}
+		if c.Regex != nil {
+			constPattern = *c.Regex
+		}
+	}
+
+	// -- if enum is defined, pick a random value from it
+	if len(constEnum) > 0 {
+		return constEnum[rand.IntN(len(constEnum))], nil
+	}
+
+	// -- if pattern is defined, generate a value matching the pattern (not implemented)
+	if constPattern != "" {
+		return reggen.Generate(constPattern, 10)
+	}
+
+	// -- otherwise return a random string
+	return "random-string" + fmt.Sprint(rand.IntN(10000)), nil
+}
---
+++ test/syntax/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sync"
+)
+
+func main() {
+	run(".connect")
+}
+
+func run(dir string) {
+	filenames, err := generateSpecFilenames(dir)
+	if err != nil {
+		fmt.Printf("❌Error generating spec filenames: %v\n", err)
+		os.Exit(1)
+	}
+
+	errDir, err := os.MkdirTemp("", "syntax-errors-")
+	if err != nil {
+		fmt.Printf("❌Error creating temp dir for errors: %v\n", err)
+		os.Exit(1)
+	}
+
+	tester := NewTester(errDir)
+
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, 5)
+
+	for _, filename := range filenames {
+		wg.Add(1)
+		sem <- struct{}{} // acquire a slot
+		go func(tester *Tester, fn string) {
+			defer wg.Done()
+			defer func() { <-sem }() // release the slot
+
+			if err := tester.TestComponent(filename); err != nil {
+				fmt.Printf("❌%v\n", err)
+				return
+			}
+
+			//fmt.Printf("✅%s\n", fn)
+		}(tester, filename)
+	}
+	wg.Wait()
+}
+
+func generateSpecFilenames(dir string) ([]string, error) {
+	sourcesDir := dir + "/sources"
+	sinksDir := dir + "/sinks"
+
+	sourceSpecs, err := listSpecsInDir(sourcesDir)
+	if err != nil {
+		return nil, err
+	}
+
+	sinkSpecs, err := listSpecsInDir(sinksDir)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("🆗Found %d source specs and %d sink specs\n", len(sourceSpecs), len(sinkSpecs))
+
+	return append(sourceSpecs, sinkSpecs...), nil
+}
+
+func listSpecsInDir(dir string) ([]string, error) {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var specs []string
+	for _, file := range files {
+		if !file.IsDir() && (len(file.Name()) > 5 && file.Name()[len(file.Name())-5:] == ".yaml" || len(file.Name()) > 4 && file.Name()[len(file.Name())-4:] == ".yml") {
+			specs = append(specs, dir+"/"+file.Name())
+		}
+	}
+	return specs, nil
+}
